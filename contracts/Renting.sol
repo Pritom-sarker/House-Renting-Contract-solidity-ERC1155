d1b94afc4306d893537b4661f1321824432d51eb
Pritom sarker Bishal <33182615+Pritom-sarker@users.noreply.github.com>
2022-04-18 03:48:36 +0600

    fix: comment added

--- contracts/Renting.sol
+++ contracts/Renting.sol
@@ -38,19 +38,24 @@ interface HouseNFTInterface {
 contract RentingContract {
     address public owner; // owner of the contract
     mapping(uint256 => rent) public listOfRenting; // list of renting property
-    uint256 public rentingID = 0;
-    address nftContractAddress;
+    uint256 public rentingID = 0; // renting index
+    address nftContractAddress; // NFT contract address
 
+    ///@notice this function is a constructor function
+    ///@param _nftContractAddress is the address of the NFT
     constructor(address _nftContractAddress) {
         owner = msg.sender;
         nftContractAddress = _nftContractAddress;
     }
 
+    // renting state
     enum statusOfRent {
         pending,
         cancel,
         complete
     }
+
+    // Rent attributes
     struct rent {
         address[] OwnerAdress;
         address renterAdress;
@@ -59,6 +64,11 @@ contract RentingContract {
         statusOfRent status;
     }
 
+    event RentNewProperty(uint256 propertyID, address renterAdress);
+    event ApproveRent(uint256 rentingId, address owner);
+
+    ///@notice Users can rent a new property by paying the renting fee using this function
+    ///@param _id is the ID of the NFT that the renter wants to rent
     function rentNewProperty(uint256 _id) public payable {
         address[] memory allOwner = HouseNFTInterface(nftContractAddress)
             .viewOwnerList(_id);
@@ -70,10 +80,15 @@ contract RentingContract {
             statusOfRent.pending
         );
         rentingID += 1;
+        emit RentNewProperty(_id, msg.sender);
     }
 
+    ///@notice this function can validate the ownership of a property(NFT)
+    ///@param _add is the address of the owner's of the property
+    ///@param _owner is the address of one of the owner of that property
+    ///@return true if the _owner is one of the owner of the property
     function checkTheValidOwner(address[] memory _add, address _owner)
-        public
+        private
         returns (bool)
     {
         for (uint256 i; i < _add.length; i++) {
@@ -84,6 +99,9 @@ contract RentingContract {
         return false;
     }
 
+    ///@notice this function can will return the renting request of that property
+    ///@param _propertyId is the ID of the property
+    ///@return the renting request of that property
     function getRentingRequest(uint256 _propertyId)
         public
         view
@@ -107,6 +125,8 @@ contract RentingContract {
         }
     }
 
+    ///@notice this function can approve the renting request of that property,only owner can call this function
+    ///@param _rentingId is the ID of the renting list
     function approveRent(uint256 _rentingId) public {
         require(
             checkTheValidOwner(
@@ -128,8 +148,11 @@ contract RentingContract {
             uint256 amount = (listOfRenting[_rentingId].price * percent) / 100;
             payable(allOwner[i]).transfer(amount);
         }
+        emit ApproveRent(_rentingId, msg.sender);
     }
 
+    ///@notice this function can cancel the renting request of that property,only owner can call this function
+    ///@param _rentingId is the ID of the renting list
     function cancelRent(uint256 _rentingId) public {
         require(
             checkTheValidOwner(
@@ -144,6 +167,8 @@ contract RentingContract {
         );
     }
 
+    ///@notice this function is used to show the current balance of the function
+    ///@return it returns the total balance of the contract
     function contractBalance() public view returns (uint256) {
         return address(this).balance;
     }
